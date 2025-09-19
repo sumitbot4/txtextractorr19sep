76546551009687047ee159ec201059ec37640b24
sumitbot4 <870.sumit123@gmail.com>
2025-09-19 13:28:55 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 web_app = Flask(__name__)
 
 @web_app.route('/')
 def home():
-    return "Bot is running by SPIDY UNIVERSE"
+    return "Bot is running by Sam"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-  
+    # Correct variable name
+    web_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
