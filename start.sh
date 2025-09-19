0f7d06de2e937f0687d85a48695f9a632edd7e2e
sumitbot4 <870.sumit123@gmail.com>
2025-09-19 12:28:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
-cd /TXT-EXTRACTOR
-pip install -r requirements.txt
+#!/bin/bash
+set -e
+
 echo "Starting Bot..."
-python -m Extractor
+python3 -m extractor
